31572897056f6640823c35b8253610b3cfe0e1d1
uig39354 <122456385+oumss91370@users.noreply.github.com>
2024-06-15 14:55:25 +0200

    project start

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+
+    private void Start()
+    {
+        Application.targetFrameRate = 60; // Limite les FPS à 30
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            Debug.Log("Jump");
+        }
+        if (Input.GetButtonDown("Crouch"))
+
+        {
+            crouch = true;
+        }else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+            
+    }
+
+     void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+        
+        
+    }
+}
